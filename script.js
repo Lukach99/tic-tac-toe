93d883765a6984595eeb3d6b4a23e27f64862905
Tino Lukac <lukac.tino@gmail.com>
2022-03-29 15:25:30 +0200

    adding more basic game logic

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const fields = document.querySelectorAll(".game div");
 const h1Element = document.querySelector("h1");
+const mainElement = document.querySelector("main");
+const gameElement = document.querySelector(".game");
+const playerChoiceList = document.querySelectorAll(".player-choice div div");
+const playerChoice = document.querySelector(".player-choice");
 
 const cross = `
           <span class="cross">
@@ -9,38 +13,46 @@ const cross = `
 
 const circle = `<span class="circle"> </span>`;
 
+class Player {
+  constructor(playerNum, mark) {
+    this.playerNum = playerNum;
+    this.mark = mark;
+    this.score = 0;
+  }
+}
+
+player4 = new Player(1, "x");
+player4.score += 1;
+
+console.log(player4);
 let player;
-
-const gameElement = document.querySelector("main .game");
-gameElement.remove();
-
-const playerChoiceList = document.querySelectorAll(".player-choice div div");
 
 playerChoiceList[0].innerHTML = cross;
 playerChoiceList[1].innerHTML = circle;
 
-const playerChoice = document.querySelector(".player-choice");
-console.log(playerChoice.innerHTML);
+gameElement.remove();
 
-playerChoiceList.forEach((mark) => {
-  mark.addEventListener("click", (event) => {
-    if (mark.innerHTML === cross) {
-      player = cross;
-      playerChoice.style.opacity = "0";
-      setTimeout(() => playerChoice.remove(), 200);
+let player1;
 
-      gameElement.style.opacity = "1";
-      setTimeout(() => document.querySelector("main").append(gameElement), 200);
-    } else if (mark.innerHTML === circle) {
-      player = circle;
-      playerChoice.style.opacity = "0";
-      setTimeout(() => playerChoice.remove(), 200);
+player1mark();
 
-      gameElement.style.opacity = "1";
-      setTimeout(() => document.querySelector("main").append(gameElement), 200);
-    }
+function player1mark() {
+  playerChoiceList.forEach((mark) => {
+    mark.addEventListener("click", (event) => {
+      if (mark.innerHTML === cross) {
+        player = cross;
+        mainElement.prepend(gameElement);
+        playerChoice.remove();
+        player1 = cross;
+      } else if (mark.innerHTML === circle) {
+        player = circle;
+        playerChoice.remove();
+        mainElement.prepend(gameElement);
+        player1 = circle;
+      }
+    });
   });
-});
+}
 
 fields.forEach((field) => {
   field.addEventListener("click", (event) => {
@@ -62,24 +74,11 @@ function addMark(position) {
   }
 }
 
-/* function gameLogic(player) {
-  switch (player) {
-    case fields[0].innerHTML === player &&
-      fields[1].innerHTML === player &&
-      fields[2].innerHTML === player:
-      if (player === player1) {
-        console.log("succes 1");
-      } else {
-        console.log("succes 2");
-      }
-  }
-} */
-
 function gameLogic(player) {
   /* horizontal */
   if (gameLogicDivs(0, 1, 2)) {
-    whoWon(player);
     winLine("top");
+    whoWon(player);
   } else if (gameLogicDivs(3, 4, 5)) {
     whoWon(player);
     winLine("middle");
@@ -108,12 +107,18 @@ function gameLogic(player) {
     whoWon(player);
     winLine("lb-rt");
   }
-}
 
+  /* checking if its draw */
+  if (checkDraw() === 0 && h1Element.innerText === "Tic-Tac-Toe") {
+    document.querySelector("h1").innerText = "its draw";
+    restartButton();
+  }
+}
+const lineElement = document.createElement("span");
 function winLine(position) {
-  const lineElement = document.createElement("span");
   lineElement.classList.add(`line-${position}`);
-  document.querySelector("main .game").prepend(lineElement);
+
+  gameElement.prepend(lineElement);
 }
 
 function gameLogicDivs(index1, index2, index3) {
@@ -126,8 +131,38 @@ function gameLogicDivs(index1, index2, index3) {
 
 function whoWon(player) {
   if (player === cross) {
-    document.querySelector("h1").innerText = "Player 1 WINS";
-  } else {
-    document.querySelector("h1").innerText = "Player 2 WINS";
+    h1Element.innerText = "Player 1 WINS";
+    restartButton();
+  } else if (player === circle) {
+    h1Element.innerText = "Player 2 WINS";
+    restartButton();
   }
+}
+
+function checkDraw() {
+  let emptyPos = 0;
+  fields.forEach((field) => {
+    if (field.innerHTML === "") {
+      emptyPos++;
+    }
+  });
+  return emptyPos;
+}
+console.log(fields);
+
+function restartButton() {
+  const restartBtnElement = document.createElement("button");
+  restartBtnElement.innerText = "Play Again?";
+  mainElement.prepend(restartBtnElement);
+
+  restartBtnElement.addEventListener("click", (event) =>
+    fields.forEach((field) => {
+      field.innerHTML = ``;
+      lineElement.className = "";
+      lineElement.remove();
+      restartBtnElement.remove();
+      h1Element.innerText = "Tic-Tac-Toe";
+      player = player1;
+    })
+  );
 }
